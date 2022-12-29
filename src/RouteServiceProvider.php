345096b2f0015709d3ada26f4b80b0cf336654b5
share <ybzc.gg@gmail.com>
2022-12-29 16:05:01 +0800

    fixup

---
+++ src/RouteServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+namespace Ybzc\Laravel\Dashboard;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    private $_packageTag = 'dashboard';
+
+    public function register()
+    {
+        parent::register();
+    }
+
+
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+        $this->getRoutes()->refreshNameLookups();
+        Route::aliasMiddleware("dashboard.view", DashboardView::class);
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapWebRoutes();
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all receive session state, CSRF protection, etc.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        if($route = $this->getRoutes()->getByName("dashboard")) {
+            Log::debug("attach middleware->".DashboardView::class.", ON->".static::class.", line->".__LINE__);
+            $route->middleware(['dashboard.view']);
+        }
+    }
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,60 +1,29 @@
 <?php
+
 namespace Ybzc\Laravel\Dashboard;
 
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Route;
-
-class ServiceProvider extends RouteServiceProvider
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    private $_packageTag = 'dashboard';
-
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
-        parent::register();
+        $this->app->register(RouteServiceProvider::class);
         $this->loadViewsFrom(__DIR__ . '/resources/views/', $this->_packageTag);
     }
 
-
     /**
-     * Called before routes are registered.
-     *
-     * Register any model bindings or pattern based filters.
+     * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
     {
-        parent::boot();
-        $this->getRoutes()->refreshNameLookups();
-        Route::aliasMiddleware("dashboard.view", DashboardView::class);
         $this->publishes([
             __DIR__ . '/config/adminlte.php' => config_path('adminlte.php'),
         ],'dashboard');
-    }
-
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map()
-    {
-        $this->mapWebRoutes();
-    }
-
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
-    {
-        if($this->getRoutes()->getByName("dashboard")) {
-            Log::debug("attach middleware->".DashboardView::class.", ON->".static::class.", line->".__LINE__);
-            $this->getRoutes()->getByName("dashboard")->middleware(['dashboard.view']);
-        }
     }
 }
